166440cd38856c919d8f62117cf2e3ff5cfe1264
Samul <samulshrestha101@gmail.com>
2020-07-24 16:18:25 +0545

    Updates

---
+++ precache-manifest.711e807d0e118535592b6a319ae2fe3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f581fb822aaf6d6de350925f25e0b9f",
+    "url": "/instagram-clone/index.html"
+  },
+  {
+    "revision": "e8c358e3737ccd23e106",
+    "url": "/instagram-clone/static/css/main.dc79bc56.chunk.css"
+  },
+  {
+    "revision": "e4b1f011b692b2cd6e48",
+    "url": "/instagram-clone/static/js/2.50d69f38.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/instagram-clone/static/js/2.50d69f38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8c358e3737ccd23e106",
+    "url": "/instagram-clone/static/js/main.f6c1cb02.chunk.js"
+  },
+  {
+    "revision": "aef9a438bf5af9aaca69",
+    "url": "/instagram-clone/static/js/runtime-main.2ae361b3.js"
+  }
+]);
